a1f0e4fb69a66fb9f3e7e0694b9efc59edff1bf1
whwogus1213 <whwogus1213@gmail.com>
2019-06-18 18:23:16 +0900

    Revert "mysql연동, 예시설정"
    This reverts commit f743113506a407e53b625fa8878117ef33091813.

--- Youditor/src/main/java/com/good/dao/AccountsDAO.java
+++
@@ -1,9 +0,0 @@
-package com.good.dao;
-
-import java.util.List;
-import com.good.dto.AccountsVO;
-
-public interface AccountsDAO {
-
-	public List<AccountsVO> selectAccounts() throws Exception;
-}
--- Youditor/src/main/java/com/good/service/AccountsService.java
+++
@@ -1,9 +0,0 @@
-package com.good.service;
-
-import java.util.List;
-
-import com.good.dto.AccountsVO;
-
-public interface AccountsService {
-	 public List<AccountsVO> selectAccounts() throws Exception;
-}
